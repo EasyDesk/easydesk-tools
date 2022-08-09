d26ca75b6b1bfd4dee68390ae77c0b710d06a225
francescodente <dente.francesco1996@gmail.com>
2022-08-10 01:46:36 +0200

    Improve file naming convention

---
+++ src/EasyDesk.Tools/Options/Option.TryConstructFactories.cs
@@ -0,0 +1,14 @@
+﻿namespace EasyDesk.Tools;
+
+public static partial class StaticImports
+{
+    public delegate bool TryConstruct<T>(out T output);
+
+    public delegate bool TryConstruct<T1, T>(T1 arg1, out T output);
+
+    public static Option<T> FromTryConstruct<T1, T>(T1 arg1, TryConstruct<T1, T> tryConstruct) =>
+        tryConstruct(arg1, out var output) ? Some(output) : None;
+
+    public static Option<T> FromTryConstruct<T>(TryConstruct<T> tryConstruct) =>
+        tryConstruct(out var output) ? Some(output) : None;
+}
