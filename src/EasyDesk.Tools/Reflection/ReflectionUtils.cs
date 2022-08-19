74b9f420df1ce01e39db78981d0058a01c97365b
francescodente <dente.francesco1996@gmail.com>
2022-08-19 12:09:52 +0200

    Improve assembly scanning API

--- src/EasyDesk.Tools/Reflection/ReflectionUtils.cs
+++ src/EasyDesk.Tools/Reflection/ReflectionUtils.cs
@@ -1,85 +1,34 @@
-﻿using System.Reflection;
-
-namespace EasyDesk.Tools.Reflection;
+﻿namespace EasyDesk.Tools.Reflection;
 
 public static class ReflectionUtils
 {
-    public static IEnumerable<T> InstancesOfSubtypesOf<T>(params Type[] assemblyTypes) =>
-        InstancesOfSubtypesOf<T>(assemblyTypes.AsEnumerable());
-
-    public static IEnumerable<Type> InstantiableSubtypesOf<T>(params Type[] assemblyTypes) =>
-        InstantiableSubtypesOf<T>(assemblyTypes.AsEnumerable());
-
-    public static IEnumerable<T> InstancesOfSubtypesOf<T>(IEnumerable<Type> assemblyTypes) =>
-        InstancesOfSubtypes(typeof(T), assemblyTypes).Cast<T>();
-
-    public static IEnumerable<Type> InstantiableSubtypesOf<T>(IEnumerable<Type> assemblyTypes) =>
-        InstantiableSubtypes(typeof(T), assemblyTypes);
-
-    public static IEnumerable<Type> InstantiableSubtypes(Type baseType, params Type[] assemblyTypes) =>
-        InstantiableSubtypes(baseType, assemblyTypes.AsEnumerable());
-
-    public static IEnumerable<object> InstancesOfSubtypes(Type baseType, params Type[] assemblyTypes) =>
-        InstancesOfSubtypes(baseType, assemblyTypes.AsEnumerable());
-
-    public static IEnumerable<Type> InstantiableTypesInAssemblies(params Type[] assemblyTypes) =>
-        InstantiableTypesInAssemblies(assemblyTypes.AsEnumerable());
-
-    public static IEnumerable<Type> InstantiableTypesInAssemblies(IEnumerable<Type> assemblyTypes)
+    public static bool IsSubtypeOrImplementationOf(this Type type, Type otherType)
     {
-        return assemblyTypes
-            .Select(t => t.Assembly)
-            .SelectMany(assembly => assembly.GetTypes())
-            .Where(t => !(t.IsAbstract || t.IsInterface));
+        return type.IsGenericTypeDefinition || otherType.IsGenericTypeDefinition
+            ? type.IsSubtypeOrImplementationOfOpenGeneric(otherType)
+            : type.IsAssignableTo(otherType);
     }
 
-    public static IEnumerable<Type> InstantiableSubtypes(Type baseType, IEnumerable<Type> assemblyTypes)
+    private static bool IsSubtypeOrImplementationOfOpenGeneric(this Type type, Type otherType)
     {
-        return InstantiableTypesInAssemblies(assemblyTypes)
-            .Where(t => t.IsAssignableTo(baseType));
-    }
-
-    public static IEnumerable<object> InstancesOfSubtypes(Type baseType, IEnumerable<Type> assemblyTypes)
-    {
-        return InstantiableSubtypes(baseType, assemblyTypes)
-            .Select(Activator.CreateInstance);
-    }
-
-    public static IEnumerable<(Type Interface, Type Implementation)> InstantiableSubtypesOfGenericInterface(
-        Type genericInterfaceType, params Type[] assemblyTypes) =>
-        InstantiableSubtypesOfGenericInterface(genericInterfaceType, assemblyTypes.AsEnumerable());
-
-    public static IEnumerable<(Type Interface, Type Implementation)> InstantiableSubtypesOfGenericInterface(
-        Type genericInterfaceType, IEnumerable<Type> assemblyTypes)
-    {
-        if (!genericInterfaceType.IsGenericTypeDefinition)
+        if (type.GetInterfaces().Any(i => i.HasSameGenericTypeDefinitionAs(otherType)))
         {
-            throw new ArgumentException("The given interface type is not an open generic", nameof(genericInterfaceType));
+            return true;
         }
-        return InstantiableTypesInAssemblies(assemblyTypes)
-            .SelectMany(impl => FindGenericImplementedInterfaces(impl, genericInterfaceType).Select(i => (i, impl)));
+        return type.IsSubtypeOfOpenGeneric(otherType);
     }
 
-    private static IEnumerable<Type> FindGenericImplementedInterfaces(Type implementationType, Type genericInterfaceType)
+    private static bool IsSubtypeOfOpenGeneric(this Type type, Type otherType)
     {
-        return implementationType
-            .GetTypeInfo()
-            .GetInterfaces()
-            .Where(i => i.IsGenericType && i.GetGenericTypeDefinition() == genericInterfaceType);
-    }
-
-    public static object GetPropertyFromPath(object source, string path)
-    {
-        static object GetPropertyFromPath(object source, Type currentType, IEnumerable<string> path)
+        if (type.HasSameGenericTypeDefinitionAs(otherType))
         {
-            var property = path.FirstOrDefault();
-            if (property == null)
-            {
-                return source;
-            }
-            var propertyInfo = currentType.GetProperty(property);
-            return GetPropertyFromPath(propertyInfo.GetValue(source), propertyInfo.PropertyType, path.Skip(1));
+            return true;
         }
-        return GetPropertyFromPath(source, source.GetType(), path.Split("."));
+        return type.BaseType is Type baseType && baseType.IsSubtypeOfOpenGeneric(otherType);
     }
+
+    private static bool HasSameGenericTypeDefinitionAs(this Type type, Type otherType) =>
+        type.IsGenericType &&
+        otherType.IsGenericType &&
+        type.GetGenericTypeDefinition() == otherType.GetGenericTypeDefinition();
 }
