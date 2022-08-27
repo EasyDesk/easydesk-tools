bc617ffb339a25a60eb3859790eb8b3e5d03fa3c
francescodente <dente.francesco1996@gmail.com>
2022-08-27 17:09:23 +0200

    Add async enumerable factories and operators

--- src/EasyDesk.Tools/Collections/AsyncEnumerableUtils.cs
+++ src/EasyDesk.Tools/Collections/AsyncEnumerableUtils.cs
@@ -10,11 +10,80 @@ public static class AsyncEnumerableUtils
         }
     }
 
-    public static async Task ForEach<T>(this IAsyncEnumerable<T> sequence, System.Action<T> action)
+    public static async Task ForEach<T>(this IAsyncEnumerable<T> sequence, Action<T> action)
     {
         await foreach (var item in sequence)
         {
             action(item);
+        }
+    }
+
+    public static async IAsyncEnumerable<R> Select<T, R>(this IAsyncEnumerable<T> sequence, Func<T, R> mapper)
+    {
+        await foreach (var item in sequence)
+        {
+            yield return mapper(item);
+        }
+    }
+
+    public static async IAsyncEnumerable<T> Where<T>(this IAsyncEnumerable<T> sequence, Func<T, bool> predicate)
+    {
+        await foreach (var item in sequence)
+        {
+            if (predicate(item))
+            {
+                yield return item;
+            }
+        }
+    }
+
+    public static async IAsyncEnumerable<R> SelectMany<T, R>(this IAsyncEnumerable<T> sequence, Func<T, IAsyncEnumerable<R>> mapper)
+    {
+        await foreach (var item in sequence)
+        {
+            await foreach (var mapped in mapper(item))
+            {
+                yield return mapped;
+            }
+        }
+    }
+
+    public static IAsyncEnumerable<T> Create<T>(Func<CancellationToken, IAsyncEnumerator<T>> enumeratorProvider) =>
+        new AsyncEnumerableImpl<T>(enumeratorProvider);
+
+    private sealed class AsyncEnumerableImpl<T> : IAsyncEnumerable<T>
+    {
+        private readonly Func<CancellationToken, IAsyncEnumerator<T>> _enumeratorProvider;
+
+        public AsyncEnumerableImpl(Func<CancellationToken, IAsyncEnumerator<T>> enumeratorProvider)
+        {
+            _enumeratorProvider = enumeratorProvider;
+        }
+
+        public IAsyncEnumerator<T> GetAsyncEnumerator(CancellationToken cancellationToken = default) =>
+            _enumeratorProvider(cancellationToken);
+    }
+
+    public static IAsyncEnumerable<T> AsAsyncEnumerable<T>(this IEnumerable<T> sequence) =>
+        Create(_ => new AsyncEnumeratorFromEnumerator<T>(sequence.GetEnumerator()));
+
+    private sealed class AsyncEnumeratorFromEnumerator<T> : IAsyncEnumerator<T>
+    {
+        private readonly IEnumerator<T> _enumerator;
+
+        public AsyncEnumeratorFromEnumerator(IEnumerator<T> enumerator)
+        {
+            _enumerator = enumerator;
+        }
+
+        public ValueTask<bool> MoveNextAsync() => ValueTask.FromResult(_enumerator.MoveNext());
+
+        public T Current => _enumerator.Current;
+
+        public ValueTask DisposeAsync()
+        {
+            _enumerator.Dispose();
+            return ValueTask.CompletedTask;
         }
     }
 }
